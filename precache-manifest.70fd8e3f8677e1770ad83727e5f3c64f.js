d708cd6b83730734e7557a4777443bac9cfb4981
Jeremy Capps <jeremycapps@Jeremys-MBP-2.home>
2019-08-03 04:17:39 -0400

    Updates

---
+++ precache-manifest.70fd8e3f8677e1770ad83727e5f3c64f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfbeab22e7b75538191c41f5ba6af7c",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "60405ca95bc20e71ec88",
+    "url": "/react-rolodex/static/css/main.6c4d9d47.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "60405ca95bc20e71ec88",
+    "url": "/react-rolodex/static/js/main.dac8ecfb.chunk.js"
+  },
+  {
+    "revision": "b0d288477536a6b5194e",
+    "url": "/react-rolodex/static/js/runtime~main.0066c125.js"
+  }
+]);
